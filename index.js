f2bcaa9f88f73023ede7a9f4428b88b84ed1fdb1
sunitee <suniteebajpai2@gmail.com>
2021-03-06 16:38:58 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return array;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
+
